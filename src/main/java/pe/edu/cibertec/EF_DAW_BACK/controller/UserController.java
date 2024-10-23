a1689a161ad3c9171b0f1776086cf1145728855f
Gad Josue Alva Castromonte <josuealva920@gmail.com>
2024-10-22 21:54:18 -0500

    update

--- src/main/java/pe/edu/cibertec/EF_DAW_BACK/controller/UserController.java
+++ src/main/java/pe/edu/cibertec/EF_DAW_BACK/controller/UserController.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
